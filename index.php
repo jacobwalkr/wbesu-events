cf7ef64b221f530713767c23ddab1ccef84f6d93
Jacob <jacob.walker@esendex.com>
2013-07-16 14:18:57 +0100

    Explanded index.php to split query string into associative array

--- index.php
+++ index.php
@@ -8,19 +8,37 @@ $uri_pattern = '~^/?(?P<controller>\w*)((/?(?P<action>\w*))?(/?(?P<data>(\w*/?)*
 $matches = array();
 preg_match($uri_pattern, $split_request[0], $matches);
 
-// Split the query string up
+// Split the data bits up (as in /controller/action/>data1/data2/data3<)
+$raw_request_data = $matches['data'];
+$request_data = explode('/', $raw_request_data);
+
+// Split the query string up and make it a nice associative array
 if (isset($split_request[1]))
 {
-    $query_string_split = explode('&', $split_request[1]);
+    $query_string_fragments = explode('&', $split_request[1]);
+    
+    $query_string_assoc = array();
+
+    foreach ($query_string_fragments as $fragment)
+    {
+        $split_fragment = explode('=', $fragment, 2);
+        $key = $split_fragment[0];
+        
+        if (count($split_fragment) == 2)
+        {
+            $value = $split_fragment[1];
+            $query_string_assoc[$key] = $value;
+        }
+        else
+        {
+            $query_string_assoc[$key] = true;
+        }
+    }
 }
 else
 {
-    $query_string_split = array();
+    $query_string_assoc = array();
 }
-
-// Split the data bits up (as in /controller/action/data1/data2/data3)
-$raw_request_data = $matches['data'];
-$request_data = explode('/', $raw_request_data);
 
 // Display URL fragments nicely
 echo "<br><strong>Controller:</strong><br>{$matches['controller']}<br><br>\n";
@@ -28,6 +46,6 @@ echo "<strong>Action:</strong><br>{$matches['action']}<br><br>\n";
 echo "<strong>Data:</strong><br>\n";
 print_r($request_data);
 echo "<br><br>\n<strong>Query:</strong><br>\n";
-print_r($query_string_split);
+print_r($query_string_assoc);
 
 ?>

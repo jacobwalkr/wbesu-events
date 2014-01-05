9f39b29889bf1fe407dba353ce29978c5ceb2dbd
Jacob Walker <jacob.walker94@gmail.com>
2014-01-05 23:49:17 +0000

    Better autoloading strategy in index.php, still need to pass on request

--- index.php
+++ index.php
@@ -2,14 +2,35 @@
 
 define('INDEX_DIR', dirname(__FILE__) . '/');
 
-foreach (glob('Core/*/') as $folder)
+/*
+ * Autoloader
+ */
+
+// Include everything from core because why dafuq not
+foreach (glob("*/") as $folder)
 {
-    foreach (glob($folder . '*.php') as $file)
+    foreach (glob($folder . "*.php") as $filename)
     {
-        require INDEX_DIR . $file;
+        // The downside of this being that everything must be exactly one
+        // folder deep :(. Eh, I'll figure it out later.
+        require $folder . $filename;
     }
 }
 
-Handler::Handle($_SERVER['REQUEST_URI']);
+function _load_app_class($class)
+{
+    foreach (glob("*/") as $folder)
+    {
+        foreach (glob($folder . "*.php") as $filename)
+        {
+            if (preg_match("~^/?" . $class . "*\.php", $filename) === 1)
+            {
+                require $folder . $filename;
+            }
+        }
+    }
+}
 
-?>
+spl_autoload_register('_load_app_class');
+
+// TODO: Include the Handler and go
